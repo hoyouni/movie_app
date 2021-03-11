94d7124a3e794bf7316242cbdb8b46e124f28aa0
hoyouni <sgm00006@gmail.com>
2021-03-11 18:09:10 +0900

    Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import Potato from './Potato';
+
+// jsx = HTML + JavaScript
+// component 의 첫 글자는 대문자
+// fav 라는 Food component property 의 value 들 출력
+function Food({fav}) {
+  return <h1>I love {fav}</h1>;
+}
 
 function App() {
   return (
     <div>
       <h1>Hello React</h1>
-      <Potato/>
+      <Food 
+            fav="kimchi" 
+      />
+      <Food 
+            fav="ramen" 
+      />
+      <Food 
+            fav="samgiopsal" 
+      />
+      <Food 
+            fav="chukumi" 
+      />
     </div>
   );
 }
