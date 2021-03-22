084ecc3e014b2320ab5d6eb0b170516a6fd8f636
hoyouni <sgm00006@gmail.com>
2021-03-22 15:33:58 +0900

    Styling the Movies

--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./App.css";
 
-function Movie({id, year, title, summary, poster, youtube}) {
-    return <h5>{title}</h5>;
+function Movie({ year, title, summary, poster}) {
+    return (
+    <div class="movies">
+        <img src={poster} alt={title} title={title} />
+        <div class="movie__data">
+            <h3 class="movie__title">{title}</h3>
+            <h5 class="movie__year">{year}</h5>
+            <p class="movie__summary">{summary}</p>
+        </div>
+    </div>
+    );
 }
 
 Movie.propTypes = {
@@ -10,8 +20,7 @@ Movie.propTypes = {
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired,
-    youtube: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired
 };
 
 export default Movie;
