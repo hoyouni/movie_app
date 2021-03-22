6cbd3f4e052973427bbfe2f88ba814220b0e2401
hoyouni <sgm00006@gmail.com>
2021-03-22 14:34:23 +0900

    Planning the movie component

--- src/App.js
+++ src/App.js
@@ -1,45 +1,22 @@
 import React from 'react';
-import PropTypes from "prop-types";
 
- // React.Component 를 상속받은 App 클래스 생성
- class App extends React.Component {
+class App extends React.Component {
 
-  // state 요소 추가 
   state = {
-    count: 0
+    isLoading: true,
+    movie: []
   };
 
-  // 메소드 요소 추가 후 setState 로직 
-  add = () => {
-    // 현재의 count 값을 가져와서 그 값에 로직 수행
-    this.setState(current => ({count: current.count + 1}));
-  };
-
-  minus = () => {
-    this.setState(current => ({count: current.count - 1}));
-  };
-
-  // 컴포넌트가 렌더링 되고 있다
   componentDidMount() {
-    console.log("component rendered");
+     setTimeout(() => {
+       this.setState({ isLoading:false });
+     },6000);
   }
 
-  // 컴포넌트가 업데이트 되었다.
-  componentDidUpdate() {
-    console.log("I'm updated");
+  render() {
+    const {isLoading} = this.state;
+    return <div>{isLoading ? "Loading..." : "Ready" }</div>;
   }
-
-    //setState()를 호출할 때 마다 reRender 하게됨   
-   render() {
-     console.log("I'm rendering");
-     return (
-      <div>
-        <h1>The number is : {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-     );
-   }
- }
+}
 
 export default App;
